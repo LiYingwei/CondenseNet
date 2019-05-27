57704de5756364712bb716ee15d0c9a7198a2256
LiYingwei <liyw14@163.com>
2019-05-26 23:18:45 -0700

    change to pytorch 1.0

--- main.py
+++ main.py
@@ -312,9 +312,9 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         ### Measure accuracy and record loss
         prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-        losses.update(loss.data[0], input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+        losses.update(loss.data.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+        top5.update(prec5.item(), input.size(0))
 
         ### Compute gradient and do SGD step
         optimizer.zero_grad()
@@ -359,9 +359,9 @@ def validate(val_loader, model, criterion):
 
         ### Measure accuracy and record loss
         prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
-        losses.update(loss.data[0], input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+        losses.update(loss.data.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+        top5.update(prec5.item(), input.size(0))
 
         ### Measure elapsed time
         batch_time.update(time.time() - end)
